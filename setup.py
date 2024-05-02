f45c795bb6dec5978060c2369739b88ae67c8461
SameerRanjanMansingh <sameerranjan0367@gmail.com>
2024-05-02 15:08:16 +0530

    cookiecutter added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='predict the person's shoping intrest',
+    author='SAMEER RANJAN MANSINGH',
+    license='MIT',
+)
